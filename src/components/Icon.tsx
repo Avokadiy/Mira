8aa93d2439c30a34d8140c435b1333113ae49218
Avokadiy <braaedy1@mail.ru>
2025-05-10 13:27:24 +0500

    +header

---
+++ src/components/Icon.tsx
@@ -0,0 +1,8 @@
+function Icon(props) {
+
+    return (
+        <img src={props.img} alt="" />
+    );
+}
+
+export default Icon;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,59 @@
 import logo from '../assets/images/logo.svg'
+import arrowDown from '../assets/icons/arrowDown.svg'
+import worldIcon from '../assets/icons/worldIcon.svg'
+import rightThinArrow from '../assets/icons/rightThinArrow.svg'
+import Icon from './Icon'
 
 function Header() {
     return (
         <header className="header">
-            <div className="header__menu">
-                <img className="header__menu-logo" src={logo} alt="" />
-                <div className="header__menu-links">
-                    <ul className="header__menu-link">Product</ul>
-                    <ul className="header__menu-link">Solutions</ul>
-                    <ul className="header__menu-link">Resources</ul>
-                    <ul className="header__menu-link">Enterprice</ul>
-                    <ul className="header__menu-link">Pricing</ul>
-                </div>
+            <nav className="header__menu">
+                <a href="javascript:void(0);" className="header__logo logo">
+                    <img
+                        className="logo-image"
+                        src={logo}
+                        alt="Miro"
+                    />
+                </a>
+                <ul className="header__menu-links">
+                    <li className="header__link">
+                        <span>Product</span>
+                        <Icon img={arrowDown} />
+                    </li>
+                    <li className="header__link">
+                        <span>Solutions</span>
+                        <Icon img={arrowDown} />
+                    </li>
+                    <li className="header__link">
+                        <span>Resources</span>
+                        <Icon img={arrowDown} />
+                    </li>
+                    <li className="header__link">
+                        <span>Enterprice</span>
+                    </li>
+                    <li className="header__link">
+                        <span>Pricing</span>
+                    </li>
+                </ul>
+            </nav>
 
-                <div>
-
-                </div>
+            <div className="header__right">
+                <ul className="header__right-links">
+                    <li className="header__link">
+                        <img className="header__link-image" src={worldIcon} alt="Иконка мира" />
+                        <span>EN</span>
+                    </li>
+                    <li className="header__link">
+                        <span>Contact Sales</span>
+                    </li>
+                    <li className="header__link">
+                        <span>Login</span>
+                    </li>
+                </ul>
+                <button className="button">
+                    <span>Sign up free</span>
+                    <img src={rightThinArrow} alt="Стрелка вправо" />
+                </button>
             </div>
         </header>
     )
